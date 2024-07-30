df191cd7b043b7e96dc9e99e56b514c23e6eca9a
Vladislav <167914847+IronCherry@users.noreply.github.com>
2024-07-30 20:03:24 +0300

    Update module 3_4.py

--- module 3_4.py
+++ module 3_4.py
@@ -7,6 +7,6 @@ def single_root_words(*other_words, root_words='ich'):
 
 
 result1 = single_root_words('rIch', 'riChiest', 'rICHalcum', 'cheers', 'richies')
-#result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result1)
-#print(result2)
+print(result2)
